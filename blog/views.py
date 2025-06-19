fd3f9293678cc4212180275352926c7fd615f25e
keindus8404 <defrican@gmail.com>
2025-06-19 11:30:57 +0900

    3교시

--- blog/views.py
+++ blog/views.py
@@ -5,29 +5,30 @@ from .models import Post
 # Create your views here.
 class PostListView(ListView):
     model = Post
-    template_name = '/templates/blog/post_list.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = '/templates/blog/post_detail.html'
+    template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
 class PostCreateView(CreateView):
     model = Post
-    template_name = '/templates/blog/post_form.html'
+    template_name = 'blog/post_form.html'
     fields = ['title', 'content']
     success_url = '/blog/'  # 성공 후 리다이렉트할 URL
 
 class PostUpdateView(UpdateView):
     model = Post
-    template_name = '/templates/blog/post_form.html'
+    template_name = 'blog/post_form.html'
     fields = ['title', 'content']
     success_url = '/blog/'  # 성공 후 리다이렉트할 URL
 
 class PostDeleteView(DeleteView):
-    model = Post
-    template_name = '/templates/blog/post_confirm_delete.html'
-    context_object_name = 'post'
-    success_url = '/blog/'  # 성공 후 리다이렉트할 URL
+    model = Post  # Specify the model to use
+    # 삭제를 시도할때 template_name을 지정 ,get방식으로 호출이 되어야만 해당 html이 렌더링됨
+    # post로 요청하면 삭제가 진행됨
+    template_name = 'blog/post_confirm_delete.html'  # Specify the template to use
+    success_url = '/blog'  # Redirect to the post list after successful deletion
