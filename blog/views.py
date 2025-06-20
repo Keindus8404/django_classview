2885e906f2e8d01a5a7a1099597ce827cd58688e
keindus8404 <defrican@gmail.com>
2025-06-20 10:30:11 +0900

    부트스트랩 연결 마지막

--- blog/views.py
+++ blog/views.py
@@ -3,22 +3,27 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views import View
 from .models import Post  # Import the Post model
-
+# Create your views here.
 class HomeView(View):
     template_name = 'blog/home.html'
     def get(self, request):
-        return render(request, self.template_name)
+        context = {
+            'user_id': request.session.get('user_id'),
+            'username': request.session.get('username'),
+        }
+        return render(request, self.template_name, context)
 
 class PostListView(ListView):
     model = Post  # Specify the model to use
     template_name = 'blog/post_list.html'  # Specify the template to use
     context_object_name = 'posts'  # Specify the context variable name
-
+        
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['user_id'] = self.request.session.get('user_id')
         context['username'] = self.request.session.get('username')
         return context
+
 
 class PostDetailView(DetailView):
     model = Post  # Specify the model to use
